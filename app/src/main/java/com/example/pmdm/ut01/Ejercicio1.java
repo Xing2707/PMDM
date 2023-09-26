d517eb1d0581238447d61e590a72434f067a3560
xing chen <Xing5539@gmail.com>
2023-09-26 17:54:21 +0200

    Mi primer commit

---
+++ app/src/main/java/com/example/pmdm/ut01/Ejercicio1.java
@@ -0,0 +1,16 @@
+package com.example.pmdm.ut01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmdm.R;
+
+public class Ejercicio1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ut01_activity_ejercicio1);
+    }
+}
