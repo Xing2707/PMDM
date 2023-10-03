f4044a26feede632e42842434dd1601c429daf41
xing chen <Xing5539@gmail.com>
2023-10-03 16:30:20 +0200

    Mi primer commit

--- app/src/main/java/com/example/pmdm/ut02/u2e2NombreColo.java
+++ app/src/main/java/com/example/pmdm/ut02/u2e2NombreColo.java
@@ -6,20 +6,20 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toolbar;
-import android.widget.ViewFlipper;
 
 import com.example.pmdm.R;
 
 public class u2e2NombreColo extends AppCompatActivity {
 
     Button btCambio;
-    TextView twSalida1;
-    SeekBar sbR1;
+    TextView twSalida1,twSalida2;
+    SeekBar sbR,sbG,sbB;
 
+    EditText etEntrada;
     Switch shTextBlanco;
 
     @Override
@@ -29,17 +29,42 @@ public class u2e2NombreColo extends AppCompatActivity {
 
         btCambio = findViewById(R.id.u2e2btCambio);
         twSalida1 = findViewById(R.id.u2e2twSalida1);
-        sbR1 = findViewById(R.id.u2e2sbR);
+        twSalida2 = findViewById(R.id.u2e2twSalida2);
+        etEntrada = findViewById(R.id.u2e2etEntrada1);
+        sbR = findViewById(R.id.u2e2sbR);
+        sbG = findViewById(R.id.u2e2sbG);
+        sbB = findViewById(R.id.u2e2sbB);
         shTextBlanco = findViewById(R.id.u2e2sh1);
 
-        btCambio.setOnClickListener((View v)->{
-            twSalida1.setText(Integer.toString(sbR1.getProgress()));
-            if(shTextBlanco.isChecked()){
+        btCambio.setOnClickListener((View v) -> {
+            int R, G, B, color;
+            R = sbR.getProgress();
+            G = sbG.getProgress();
+            B = sbB.getProgress();
+            color = Color.rgb(R, G, B);
+            twSalida1.setBackgroundColor(color);
+            twSalida1.setText(etEntrada.getText());
+        });
+
+        sbR.setOnSeekBarChangeListener(new sbListener());
+        sbG.setOnSeekBarChangeListener(new sbListener());
+        sbB.setOnSeekBarChangeListener(new sbListener());
+
+        shTextBlanco.setOnClickListener((View v) ->{
+            if (shTextBlanco.isChecked()) {
                 twSalida1.setTextColor(Color.parseColor("#FFFFFF"));
-            }else{
+            } else {
                 twSalida1.setTextColor(Color.parseColor("#000000"));
             }
         });
-
     }
+    private class sbListener implements SeekBar.OnSeekBarChangeListener{
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            twSalida2.setBackgroundColor(Color.rgb(sbR.getProgress(),sbG.getProgress(),sbB.getProgress()));
+        }
+        public void onStartTrackingTouch(SeekBar seekBar) {}
+
+        public void onStopTrackingTouch(SeekBar seekBar) {}
+    }
+
 }
